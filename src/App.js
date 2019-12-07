b80c07788d9661d82e8074b7e15d45f93a33eb2d
Zac Morton <zmorton8057@gmail.com>
2019-12-06 17:52:15 -0800

    updated pwa files to include service worker.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Router, browserHistory, Route } from 'react-router';
+import { Link, } from 'react-router-dom';
 import './App.css';
+const NavBar = () => (
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="navbar">
+    <Router history={browserHistory}>
+      <Link to="/">Feed</Link>
+
+      <Link to="/profile">Profile</Link>
+    </Router>
+  </div>
+
+);
+
+
+const Template = ({ title }) => (
+
+  <div>
+
+    <NavBar />
+
+    <p className="page-info">
+
+      This is the {title} page.
+
+</p>
+
+  </div>
+
+);
+
+
+const Feed = (props) => (
+
+  <Template title="Feed" />
+
+);
+
+
+const Profile = (props) => (
+
+  <Template title="Profile" />
+
+);
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <Router history={browserHistory}>
+
+        <Route path="/" component={Feed} />
+
+        <Route path="/profile" component={Profile} />
+
+      </Router>
+
+    );
+
+  }
+
 }
 
 export default App;
