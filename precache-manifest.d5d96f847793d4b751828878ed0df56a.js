66c9060fc3c9473690c99465a654c952d9cdcc4e
Zac Morton <zmorton8057@gmail.com>
2019-12-06 16:07:17 -0800

    Updates

---
+++ precache-manifest.d5d96f847793d4b751828878ed0df56a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15dbc37da325b7efcfc89f1866420f5",
+    "url": "/zmorton8057/pwa-conversions.git/index.html"
+  },
+  {
+    "revision": "05d12e86dba6f3b3a82a",
+    "url": "/zmorton8057/pwa-conversions.git/static/css/main.680726dc.chunk.css"
+  },
+  {
+    "revision": "3f861efc87395fd53f7d",
+    "url": "/zmorton8057/pwa-conversions.git/static/js/2.f727b364.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/zmorton8057/pwa-conversions.git/static/js/2.f727b364.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05d12e86dba6f3b3a82a",
+    "url": "/zmorton8057/pwa-conversions.git/static/js/main.103d9704.chunk.js"
+  },
+  {
+    "revision": "b06447129b431d2ea20a",
+    "url": "/zmorton8057/pwa-conversions.git/static/js/runtime-main.4fc0a3a0.js"
+  }
+]);
